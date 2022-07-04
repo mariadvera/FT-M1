258862d4f41287b356559c6d6b5245e29dc15aa2
mariadvera <maliave213@gmail.com>
2022-07-04 12:55:19 -0500

    homework terminada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -3,12 +3,34 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+  var array = num.split('');
+
+  var sum = 0;
+
+  for(var i=0; i<array.length; i++ ){
+    sum = sum + Math.pow(2, array.length-1-i) * array[i];
+  }
+
+  return sum;
+
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
-}
+  if( num <= 0 ) return  '00000000';
+
+  var array = [];
+
+  while(num > 0){
+
+    array.unshift(num % 2);
+    num = Math.floor(num/2);
+  }
+
+    return array.join('');
+  }
+
 
 
 module.exports = {
